c00b2d6c06e6e763e013e3c1b7dc15322a11318e
liuxiao <liuxiao2103@qq.com>
2020-09-05 23:34:28 +0800

    add 0102 solution

---
+++ LeetCode_C++/102.二叉树的层序遍历.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=cpp
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if(root) q.push(root);
+        while(!q.empty())
+        {
+            vector<int> row;
+            int sz = q.size();
+            for(int i = 0; i < sz; i++)
+            {
+                TreeNode *t = q.front();
+                q.pop();
+                row.push_back(t->val);
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            ans.push_back(row); 
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
---
+++ LeetCode_Note/0102 二叉树的层序遍历(Binary Tree Level Order Traversal).md
@@ -0,0 +1,53 @@
+# 0102 二叉树的层序遍历(Binary Tree Level Order Traversal)
+
+> 题目链接：[力扣](https://leetcode-cn.com/problems/binary-tree-level-order-traversal/) | [LeetCode](https://leetcode.com/problems/binary-tree-level-order-traversal/)
+ | [AcWing](3)
+
+## 题目
+
+给你一个二叉树，请你返回其按**层序遍历**得到的节点值。（即逐层地，从左到右访问所有节点）。
+
+示例：
+二叉树：[3,9,20,null,null,15,7],
+
+```plain
+    3
+   / \
+  9  20
+    /  \
+   15   7
+```
+
+返回其层次遍历结果：
+
+```plain
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+```
+
+---
+
+## 思路1
+
+> 时间复杂度 $O(N)$，空间复杂度 $O(N)$
+
+## 代码1
+
+```cpp
+
+```
+
+---
+
+## 思路2
+
+> 时间复杂度 $O(N)$，空间复杂度 $O(N)$
+
+## 代码2
+
+```cpp
+
+```
