079498f8933193afdfc9e3e6ecc3e841f6837bbd
liuxiao <liuxiao2103@qq.com>
2021-01-22 18:36:39 +0800

    add lc35 solution1

---
+++ LeetCode_C++/35.搜索插入位置.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=cpp
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        nums.push_back(INT_MAX);
+        int l = 0, r = nums.size() - 1;
+        while(l < r) {
+            int mid = (l + r) >> 1;
+            if(nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
+// @lc code=end
+
