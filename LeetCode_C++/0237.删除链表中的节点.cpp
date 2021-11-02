3df53561fc3f9bed486e73db76b07804ff6a31e4
liuxiao <liuxiao2103@qq.com>
2021-11-02 23:56:30 +0800

    feat: add 删除链表中的节点-C++

---
+++ LeetCode_C++/0237.删除链表中的节点.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=237 lang=cpp
+ *
+ * [237] 删除链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        auto ne = node->next;
+        node->val = ne->val;
+        node->next = ne->next;
+    }
+};
+// @lc code=end
+
