901e38643529d695f53e2a47cd8a3045416ee0dd
liuxiao <liuxiao2103@qq.com>
2021-11-15 23:46:07 +0800

    feat: add 0319-C++

---
+++ LeetCode_C++/0319.灯泡开关.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=cpp
+ *
+ * [319] 灯泡开关
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+// @lc code=end
+
