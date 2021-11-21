49f6a3e3f3b8419385f8d25a7aff919e08c1a60a
liuxiao <liuxiao2103@qq.com>
2021-11-21 23:32:54 +0800

    feat: add 0559-n 叉树的最大深度-C++

---
+++ LeetCode_C++/0559.n-叉树的最大深度.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=559 lang=cpp
+ *
+ * [559] N 叉树的最大深度
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(!root) return 0;
+        int maxV = 0;
+        for(auto &n: root->children) {
+            maxV = max(maxV, maxDepth(n));
+        }
+        return maxV + 1;
+    }
+};
+// @lc code=end
+
