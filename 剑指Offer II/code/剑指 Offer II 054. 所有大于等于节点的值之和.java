79364785e83692d1f153dd9ec80da8f4571cb341
liuxiao <liuxiao2103@qq.com>
2022-03-21 23:34:19 +0800

    feat: add 剑指 Offer II 054. 所有大于等于节点的值之和.java

---
+++ 剑指Offer II/code/剑指 Offer II 054. 所有大于等于节点的值之和.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        int sum = 0;
+        while(cur != null || !stack.isEmpty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.right;
+            }
+            cur = stack.pop();
+            sum += cur.val;
+            cur.val = sum;
+            cur = cur.left;
+        }
+        return root;
+    }
+}
