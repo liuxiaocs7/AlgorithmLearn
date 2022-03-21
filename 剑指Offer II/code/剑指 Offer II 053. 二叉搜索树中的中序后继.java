efa1de5b99142e18c35a8a6cfc365fddcdeed177
liuxiao <liuxiao2103@qq.com>
2022-03-21 23:33:47 +0800

    feat: add 剑指 Offer II 053. 二叉搜索树中的中序后继.java

---
+++ 剑指Offer II/code/剑指 Offer II 053. 二叉搜索树中的中序后继.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode cur = root;
+        TreeNode res = null;
+        while(cur != null) {
+            if(cur.val > p.val) {
+                res = cur;
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return res;
+    }
+}
