11d50830709c00a639a510630e2e659dc7ab0148
liuxiao <liuxiao2103@qq.com>
2022-03-22 23:27:48 +0800

    feat: add 剑指 Offer II 012. 左右两边子数组的和相等-Java

---
+++ 剑指Offer II/code/剑指 Offer II 012. 左右两边子数组的和相等.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(leftSum == sum - nums[i] - leftSum) {
+                return i;
+            }            
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
