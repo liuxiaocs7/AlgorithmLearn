db86c14e9196feb2f7bec5b9b852ddc070bd5580
liuxiao <liuxiao2103@qq.com>
2022-05-09 22:15:47 +0800

    feat: add 0942-增减字符串匹配-Java

--- LeetCode_Java/0942.增减字符串匹配.java
+++ LeetCode_Java/0942.增减字符串匹配.java
@@ -7,7 +7,14 @@
 // @lc code=start
 class Solution {
     public int[] diStringMatch(String s) {
-
+        int n = s.length();
+        int[] res = new int[n + 1];
+        int l = 0, r = n;
+        for(int i = 0; i < n; i++) {
+            res[i] = s.charAt(i) == 'I' ? l++ : r--;
+        }
+        res[n] = l;
+        return res;
     }
 }
 // @lc code=end
