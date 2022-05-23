1b10316be4895d36749f07662c834efdcad2a269
liuxiao <liuxiao2103@qq.com>
2022-05-23 23:56:13 +0800

    feat: add 剑指 Offer 09. 用两个栈实现队列 Java

---
+++ 剑指Offer I/剑指 Offer 09. 用两个栈实现队列.java
@@ -0,0 +1,30 @@
+class CQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+    
+    public int deleteHead() {
+        if(!stack2.isEmpty()) {
+            return stack2.pop();
+        }
+        while(!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.isEmpty() ? -1 : stack2.pop();
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
