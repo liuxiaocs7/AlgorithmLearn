dd07e39196dd84d298025dde7a56d9457f4a57b2
liuxiao <liuxiao2103@qq.com>
2022-06-02 23:33:07 +0800

    feat: add 0450-删除二叉搜索树中的节点-Java

---
+++ LeetCode_Java/0450.删除二叉搜索树中的节点.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=java
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+
+        if(root.val == key) {
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+            TreeNode t = root.left;
+            while(t.right != null) t = t.right;
+            t.right = root.right;
+            return root.left;
+        } else if(root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+}
+// @lc code=end
+
