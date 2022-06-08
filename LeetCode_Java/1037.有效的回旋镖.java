784a263c8ee615e248487a001b4cfb3e8d67f3e2
liuxiao <liuxiao2103@qq.com>
2022-06-08 23:11:49 +0800

    feat: add 1037-有效的回旋镖-Java

---
+++ LeetCode_Java/1037.有效的回旋镖.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1037 lang=java
+ *
+ * [1037] 有效的回旋镖
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        return (points[1][0] - points[0][0]) * (points[2][1] - points[0][1]) != (points[2][0] - points[0][0]) * (points[1][1] - points[0][1]);
+    }
+}
+// @lc code=end
+
