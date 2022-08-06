b6560c013873c12a50b71fa06d30a2023f7ddc80
liuxiao <liuxiao2103@qq.com>
2022-08-06 22:05:13 +0800

    feat: add 1408-数组中的字符串匹配-Java

---
+++ LeetCode_Java/1408.数组中的字符串匹配.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1408 lang=java
+ *
+ * [1408] 数组中的字符串匹配
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> res = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (words[j].indexOf(words[i]) >= 0) {
+                    res.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
