8fca270f8e54aae7cca368bd4a820770c3cd451f
liuxiao <liuxiao2103@qq.com>
2023-02-02 01:05:03 +0800

    add 0122-C++

---
+++ LeetCode_C++/0122.买卖股票的最佳时机-ii.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=cpp
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0, n = prices.size();
+        for (int i = 1; i < n; i++) {
+            int sub = prices[i] - prices[i - 1];
+            if (sub > 0) res += sub;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
