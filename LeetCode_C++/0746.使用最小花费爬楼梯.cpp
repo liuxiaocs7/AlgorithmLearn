b7d89f75294d0a1074da9c48c98d8c18bc28450a
liuxiao <liuxiao2103@qq.com>
2023-02-02 20:11:14 +0800

    add 0746-C++

---
+++ LeetCode_C++/0746.使用最小花费爬楼梯.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=cpp
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> f(n + 1);
+        for (int i = 2; i <= n; i++) {
+            f[i] = min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+        }
+        return f[n];
+    }
+};
+// @lc code=end
+
