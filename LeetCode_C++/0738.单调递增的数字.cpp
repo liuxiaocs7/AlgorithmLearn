ec502b894c0bb4456ae06fbac64d4d4ed697283c
liuxiao <liuxiao2103@qq.com>
2023-02-02 12:44:59 +0800

    add 0738-C++

---
+++ LeetCode_C++/0738.单调递增的数字.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=738 lang=cpp
+ *
+ * [738] 单调递增的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int monotoneIncreasingDigits(int n) {
+        auto str = to_string(n);
+        int k = 0;
+        while (k < str.size() - 1 && str[k] <= str[k + 1]) k++;
+        if (k == str.size() - 1) return n;
+        while (k && str[k - 1] == str[k]) k--;
+        str[k]--;
+        for (int i = k + 1; i < str.size(); i++) str[i] = '9';
+        return stoi(str);
+    }
+};
+// @lc code=end
+
